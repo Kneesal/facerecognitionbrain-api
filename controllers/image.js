41e666380ea392b978e783b962274f37ada501dc
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-01-29 09:43:42 +1300

    added validation for imageURL in /images controller

--- controllers/image.js
+++ controllers/image.js
@@ -1,9 +1,10 @@
 const hangleImage = (req, res, knex, ClarifaiStub, grpc, KEY) => {
+  if (req.body.imageURL) {
     const { id, imageURL } = req.body;
 
     const USER_ID = "kneesal";
     // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = KEY 
+    const PAT = KEY;
     const APP_ID = "facerecognitionbrain";
     // Change these to whatever model and image URL you want to use
     const MODEL_ID = "face-detection";
@@ -16,55 +17,58 @@ const hangleImage = (req, res, knex, ClarifaiStub, grpc, KEY) => {
     const metadata = new grpc.Metadata();
     metadata.set("authorization", "Key " + PAT);
     stub.PostModelOutputs(
-        {
-            user_app_id: {
-                "user_id": USER_ID,
-                "app_id": APP_ID
-            },
-            model_id: MODEL_ID,
-            version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version.
-            inputs: [
-                { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
-            ]
+      {
+        user_app_id: {
+          user_id: USER_ID,
+          app_id: APP_ID,
         },
-        metadata,
-        (err, response) => {
-            if (err) {
-                throw new Error(err);
-            }
-
-            if (response.status.code !== 10000) {
-                throw new Error("Post model outputs failed, status: " + response.status.description);
-            }
-
-            // Since we have one input, one output will exist here.
-            const clarifaiOutput = response;
-            knex('users')
-            .where('id', '=', id)
-            .increment('entries', 1)
-            .returning('entries')
-            .then(entries => {
-                res.json({
-                    entries: entries[0].entries, 
-                    clarifaiOutput: clarifaiOutput
-                })
-            })
-            .catch(err => res.status(400).json(err, ' unable to get entries'))
-            
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version.
+        inputs: [
+          { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } },
+        ],
+      },
+      metadata,
+      (err, response) => {
+        if (err) {
+          throw new Error(err);
         }
 
-    );
+        if (response.status.code !== 10000) {
+          throw new Error(
+            "Post model outputs failed, status: " + response.status.description
+          );
+        }
 
-    // knex('users')
-    // .where('id', '=', id)
-    // .increment('entries', 1)
-    // .returning('entries')
-    // .then(entries => {
-    //     res.json(entries[0].entries,)
-    // })
-    // .catch(err => res.status(400).json(err, ' unable to get entries'))
-}
+        // Since we have one input, one output will exist here.
+        const clarifaiOutput = response;
+        knex("users")
+          .where("id", "=", id)
+          .increment("entries", 1)
+          .returning("entries")
+          .then((entries) => {
+            res.json({
+              entries: entries[0].entries,
+              clarifaiOutput: clarifaiOutput,
+            });
+          })
+          .catch((err) => res.status(400).json(err, " unable to get entries"));
+      }
+    );
+  } else {
+    res.status(400).json('unable to get image URL')
+  }
+
+  // knex('users')
+  // .where('id', '=', id)
+  // .increment('entries', 1)
+  // .returning('entries')
+  // .then(entries => {
+  //     res.json(entries[0].entries,)
+  // })
+  // .catch(err => res.status(400).json(err, ' unable to get entries'))
+};
 
 module.exports = {
-    hangleImage: hangleImage
-}
+  hangleImage: hangleImage,
+};
